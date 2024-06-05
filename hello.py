59c2e28a068280598306ba1280734fcd6f242ee2
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-05 14:26:38 +0100

    Functions

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
-# Ask user for their name
-name = input ("What's your name? ").strip().title()
+# Define main function
+def main():
+    name = input ("What's your name? ").strip().title()
+    # Split user's name into first and last name
+    first, last = name.split(" ")
+    hello(first)
 
-# Say hello to user
-print(f"hello, {name}")
+# Define function hello()
+def hello(to="world"):
+    print("hello, ", to)
+
+
+main()
