d4c6aec9dc8a74fda6b1f70bbbcb42770b7ac0ee
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-11 16:52:52 +0100

    functions

--- cat.py
+++ cat.py
@@ -12,9 +12,17 @@ while i<3:
     print("meow")"""
 
 # Validating input
-while True:
-    n=int(input("How many meows? "))
-    if n>0:
-        break
-for _ in range(n):
-    print("meow")
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n=int(input("How many meows? "))
+        if n>0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
