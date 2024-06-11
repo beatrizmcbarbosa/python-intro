1873f27f8f0839c99ff63cf04992711bf2072435
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-11 16:47:14 +0100

    Added patronus

--- hogwarts.py
+++ hogwarts.py
@@ -1,12 +1,18 @@
-students={
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-}
+students=[
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Eussel terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
+# Print name, house, patronus
 for student in students:
-    print(student, students[student], sep=", house: ")
+    print(student["name"], student["house"], student["patronus"], sep= ", ")
+
+# Print name and house (there was no patronus)
+"""for student in students:
+    print(student, students[student], sep=", house: ")"""
+
 # Print existing students (there was no house")
 """for student in students:
     print(student)"""
