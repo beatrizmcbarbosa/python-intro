bbb99214d9cd131a3b326f54f8df1e9bd310616b
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-11 16:38:39 +0100

    Added students' houses

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,16 @@
-students=["Hermione", "Harry", "Ron"]
+students={
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
 
 for student in students:
-    print(student)
+    print(student, students[student], sep=", house: ")
+# Print existing students (there was no house")
+"""for student in students:
+    print(student)"""
+
+# Print existing students (there was no house")
+"""for i in range(len(students)):
+    print(i+1, students[i])"""
