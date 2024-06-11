434e31353cf312cd02abe367821210e51fc45889
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-11 16:16:45 +0100

    For loop and validating input

--- cat.py
+++ cat.py
@@ -1,4 +1,20 @@
-i=0
+# While loop
+"""i=0
 while i<3:
     print("meow")
     i+=1
+"""
+
+# For loop: first option is hard to read, second is good
+"""print("meow\n" * 3, end="")
+"""
+"""for _ in range(3):
+    print("meow")"""
+
+# Validating input
+while True:
+    n=int(input("How many meows? "))
+    if n>0:
+        break
+for _ in range(n):
+    print("meow")
