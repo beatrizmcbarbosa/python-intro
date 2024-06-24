753dc1584522c37c351b95e3845cfa3d17480bb4
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-24 17:55:39 +0100

    Libraries: cli arguments, sys(introducing slides)

--- name.py
+++ name.py
@@ -9,8 +9,9 @@ import sys
 # The way we did it below, avoids the need to handle try - exceptions (IndexValue exception in this case)
 # If I'd like to support full name I can add "" to the CLI, e.g. python name.py "David Malan"
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too few arguments")
+
+# Introducing slice. sys.argv[1:] will give me a slice of the list, in this case, starting at index 1.
+# I could also request a slice sys.argv[1:-1] which would give me the results in index 1 up to the one before the last.
+for arg in sys.argv:
+    print("hello, my name is", arg)
