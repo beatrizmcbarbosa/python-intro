e6b6d8260e844f4a0939e6ed6b62619a811c8c0c
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-06-25 16:43:47 +0100

    Import from custom library

--- say.py
+++ say.py
@@ -1,8 +1,14 @@
-import cowsay
+"""import cowsay"""
 import sys
 
+from sayings import hello
+
 if len(sys.argv) == 2:
-    cowsay.trex("hello, " + sys.argv[1])
+    hello(sys.argv[1])
+
+# Cow saying with Trex option
+"""if len(sys.argv) == 2:
+    cowsay.trex("hello, " + sys.argv[1])"""
 
 # Cow option
 """if len(sys.argv) == 2:
