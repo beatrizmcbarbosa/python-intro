714bc49aa87066e2b22cdd3f64005c3963890d80
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-01 17:22:48 +0100

    removed main()

--- test_calculator.py
+++ test_calculator.py
@@ -2,10 +2,6 @@ import pytest
 from calculator import square
 
 
-def main():
-    test_square()
-
-
 def test_positive():
     assert square(2) == 4
     assert square(3) == 9
@@ -23,7 +19,3 @@ def test_zero():
 def test_str():
     with pytest.raises(TypeError):
         square("cat")
-
-
-if __name__ == "__main__":
-    main()
