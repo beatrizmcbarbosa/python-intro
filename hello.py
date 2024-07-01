5756deaec6d10a2937bc9b45ea4a41ddc716dc7f
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-01 17:27:46 +0100

    Unit Tests

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 # Define main function
 def main():
-    name = input ("What's your name? ").strip().title()
+    name = input("What's your name? ").strip().title()
     # Split user's name into first and last name
-    first, last = name.split(" ")
-    hello(first)
+    """first, last = name.split(" ")"""
+    hello(name)
+
 
 # Define function hello()
 def hello(to="world"):
     print("hello, ", to)
 
 
-main()
+if __name__ == "__main__":
+    main()
