ef5476e3a04bb1f6df24670637f3101535e35937
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-06 12:37:38 +0100

    introducting sort keys

--- students.py
+++ students.py
@@ -1,4 +1,19 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        # Using rows
+        """row = line.rstrip().split(",")
+        print(f"{row[0]} is in {row[1]}")"""
+        # Naming the rows' arguments
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name, reverse=True):
+    print(f"{student['name']} is in {student['house']}")
