ac631a43d4547a17f6285d615727b03b2f05667e
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-06 17:34:20 +0100

    Lambda: anonymous functions

--- students.py
+++ students.py
@@ -10,10 +10,9 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
-
-def get_name(student):
-    return student["name"]
-
-
-for student in sorted(students, key=get_name, reverse=True):
+# Lambda introduces the concept of anonymous functions.
+# You can use lambda as many times as you want.
+# If lambda requires multiple paramenters, you can use commas, e.g.:
+# lambda student, x, y: ...
+for student in sorted(students, key=lambda student: student["name"], reverse=True):
     print(f"{student['name']} is in {student['house']}")
