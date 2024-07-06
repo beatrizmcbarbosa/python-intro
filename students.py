2a5ef395dbc3648846a37d1d2bf7a0ece4b3047f
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-06 18:08:31 +0100

    csv Reader

--- students.py
+++ students.py
@@ -1,18 +1,15 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        # Using rows
-        """row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")"""
-        # Naming the rows' arguments
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
 
 # Lambda introduces the concept of anonymous functions.
 # You can use lambda as many times as you want.
 # If lambda requires multiple paramenters, you can use commas, e.g.:
 # lambda student, x, y: ...
-for student in sorted(students, key=lambda student: student["name"], reverse=True):
-    print(f"{student['name']} is in {student['house']}")
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
