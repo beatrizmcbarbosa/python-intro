8e3a4cfc9c76f2ba6c4898333150ff56fdc680ad
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-06 18:14:50 +0100

    DictReader

--- students.py
+++ students.py
@@ -3,13 +3,17 @@ import csv
 students = []
 
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append(
+            {"name": row["name"], "home": row["home"], "house": row["house"]}
+        )
 
 # Lambda introduces the concept of anonymous functions.
 # You can use lambda as many times as you want.
 # If lambda requires multiple paramenters, you can use commas, e.g.:
 # lambda student, x, y: ...
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    print(
+        f"{student['name']} is from {student['home']} and now lives in {student['house']}"
+    )
