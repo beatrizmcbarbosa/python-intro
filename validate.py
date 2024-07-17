4a64b8c7e7b17b23fa20415e6b210882f1bdc941
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-17 10:27:25 +0100

    Groups

--- validate.py
+++ validate.py
@@ -2,7 +2,8 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
+# What is in the parenthesis, (\w+\.)? can either be there once or not at all
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
