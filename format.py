346edea6217915ee246c0aa6101148fd45697572
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-18 13:30:19 +0100

    Walrus operator

--- format.py
+++ format.py
@@ -1,8 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
-# Group(s) does NOT start counting in 0, it starts in 1
-if matches:
+if matches := re.search(r"^(.+), (.+)$", name):
+    # Group(s) does NOT start counting in 0, it starts in 1
     name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
