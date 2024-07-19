fd1e4bb6503424d4bd4e03fcd336d05bbec19ef3
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-19 10:45:08 +0100

    Patterns

--- code.py
+++ code.py
@@ -2,7 +2,14 @@ import re
 
 
 def main():
-    code = input("Hexadecimal color code: ")
+    code = input("Hexadecimal colour code: ")
+
+    pattern = r"^#[a-fA-F0-9]{6}$"
+    match = re.search(pattern, code)
+    if match:
+        print(f"Valid. Matched with {match.group()}")
+    else:
+        print("Invalid")
 
 
 main()
