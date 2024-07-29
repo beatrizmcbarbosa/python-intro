787af148c09ebea29bb4886360b622a5c924c158
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-29 21:33:00 +0100

    if statement assigns house regardless of input

--- student.py
+++ student.py
@@ -1,5 +1,7 @@
 def main():
     student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
     # Can also represent a tuple like this. When to use tuple vs list? When I know the value can change.
     print(f"{student[0]} from {student[1]}")
 
@@ -8,7 +10,7 @@ def get_student():
     name = input("Name: ")
     house = input("House: ")
     # Here I am returning one value which is a tuple. And within this tupple, there are two values.
-    return (name, house)
+    return [name, house]
 
 
 if __name__ == "__main__":
