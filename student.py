dcae2df41efcb6f38e32116a9dfb89b7d667c0f0
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-29 22:01:04 +0100

    Classes and Objects

--- student.py
+++ student.py
@@ -1,15 +1,17 @@
+class Student: ...
+
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    # Can also represent a tuple like this. When to use tuple vs list? When I know the value can change.
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
+    # An object is the same as an instance
     name = input("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student(name, house)
+    return student
 
 
 if __name__ == "__main__":
