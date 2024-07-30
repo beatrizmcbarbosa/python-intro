3bdad36b32dd12d93c3e5157e20a8873de001528
Beatriz Barbosa <beatriz.barbosa@instanda.com>
2024-07-30 12:06:59 +0100

    name property, getter, setter

--- student.py
+++ student.py
@@ -1,12 +1,20 @@
 class Student:
-    def __init__(self, name, house, patronus):
-        if not name:
-            raise ValueError("Missing name")
+    def __init__(self, name, house):
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
 
     # Getter
     @property
