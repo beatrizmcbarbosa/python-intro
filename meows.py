a33693edeb7cd3901943e9e1e15e9aab5a037678
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-07 18:53:21 +0100

    Docstring

--- meows.py
+++ meows.py
@@ -1,5 +1,15 @@
-# Here I am saying that meow will return a value which will be a string
 def meow(n: int) -> str:
+    # Below, docstring, is a string used to document a Python module, class, function or method
+    # It is just a convention, in standard format, to document, in this case, my function
+    """
+    Meow n times.
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
     return "meow\n" * n
 
 
