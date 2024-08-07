fc2865cf2e46db1f22dba9b924929813306e5ea4
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-07 19:27:18 +0100

    Added a comment to for loop

--- meows.py
+++ meows.py
@@ -5,5 +5,6 @@ parser = argparse.ArgumentParser(description="Meow like a cat")
 parser.add_argument("-n", default=1, help="number of times to meow", type=int)
 args = parser.parse_args()
 
+# args.n contains the integer that the human types after a space after -n
 for _ in range(int(args.n)):
     print("meow")
