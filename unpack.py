774716af6816d460ecc6dafcb2b9425749867507
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-08 12:55:21 +0100

    Added comments on args and kwargs

--- unpack.py
+++ unpack.py
@@ -1,5 +1,7 @@
+# args is used to pass a variable number of arguments to a function.
+# kwargs is a special syntax that allows you to pass a keyworded, variable-length argument dictionary to a function
 def f(*args, **kwargs):
     print("Positional:", kwargs)
 
 
-f(galleons=100, sickles=50, knuts=25)
+f(galleons=100, sickles=50, knuts=25, 5)
