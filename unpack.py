673580c9fc88867dbf6e1bf135016c12ad2561c7
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-08 12:47:27 +0100

    Unpacking positional args

--- unpack.py
+++ unpack.py
@@ -1,9 +1,5 @@
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
+def f(*args, **kwargs):
+    print("Positional:", args)
 
 
-coins = [100, 50, 25]
-
-# By using * in *coins, I am unpacking in individual members of the coins list
-# * can take a data structure and unpack it
-print(total(*coins), "Knuts")
+f(100, 50, 25)
