fef231984d9edba419ea676ba8372a1b748be7e3
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-09 13:27:06 +0100

    Update functionality

--- gryffindors.py
+++ gryffindors.py
@@ -1,6 +1,13 @@
-strudents = [
+students = [
     {"name": "Hermione", "house": "Gryffindor"},
     {"name": "Harry", "house": "Gryffindor"},
     {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco", "house": "Slytherin"}
+    {"name": "Draco", "house": "Slytherin"},
 ]
+
+gryffindors = [
+    student["name"] for student in students if student["house"] == "Gryffindor"
+]
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindors)
