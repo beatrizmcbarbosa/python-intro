58f3e35287da81a76066eb6a81243d569206aa3b
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-09 13:12:12 +0100

    Updated to include *args

--- yell.py
+++ yell.py
@@ -1,8 +1,8 @@
 def main():
-    yell(["This", "is", "CS50"])
+    yell("This", "is", "CS50")
 
 
-def yell(words):
+def yell(*words):
     # Initialize the variable uppercase as a list
     uppercased = []
     for word in words:
