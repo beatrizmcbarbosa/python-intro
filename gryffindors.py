26665115d856360b714306799dd4a0e4131b2316
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-09 13:23:50 +0100

    First commit

---
+++ gryffindors.py
@@ -0,0 +1,6 @@
+strudents = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"}
+]
