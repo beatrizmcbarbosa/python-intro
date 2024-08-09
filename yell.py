277182656cba8274e77e55fa11efb455bfb877e5
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-09 13:16:57 +0100

    map function

--- yell.py
+++ yell.py
@@ -3,10 +3,9 @@ def main():
 
 
 def yell(*words):
-    # Initialize the variable uppercase as a list
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+    # Here I am passing another function to map, referencing it, not calling it - in this case this is upper
+    # map will iterate over each of those word, call str.upper on each of those words, and return to me a brand new list containing all of these results
+    uppercased = map(str.upper, words)
     print(*uppercased)
 
 
