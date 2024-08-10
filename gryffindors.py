98da379e06dee1cabca21b77f69982e1c87d9731
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-10 11:35:54 +0100

    Dictionary comprehensions

--- gryffindors.py
+++ gryffindors.py
@@ -1,21 +1,9 @@
-students = [
-    {"name": "Hermione", "house": "Gryffindor"},
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco", "house": "Slytherin"},
-]
+students = ["Hermione", "Harry", "Ron"]
 
 
-def is_gryffindor(s):
-    # Here I am returning the value of the boolean in a simplified manner
-    # If the student house is gryffindor, return. If False, do nothing. That's basically what it's saying
-    return s["house"] == "Gryffindor"
+gryffindors = []
 
+for student in students:
+    gryffindors.append({"name": student, "house": "Gryffindor"})
 
-# Similar to map, I am passing a function that is going to apply to every student in my list
-# The filter() function returns an iterator where the items are filtered through a function to test if the item is accepted or not
-gryffindors = filter(is_gryffindor, students)
-
-# I can sort a list in a dictionary using the below syntax
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    print(gryffindor["name"])
+print(gryffindors)
