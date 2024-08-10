533b2d2e061891bfb0336475247c51dd3dc3b682
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-10 11:41:37 +0100

    Dictionary comprehensions 2

--- gryffindors.py
+++ gryffindors.py
@@ -1,9 +1,6 @@
 students = ["Hermione", "Harry", "Ron"]
 
 
-gryffindors = []
-
-for student in students:
-    gryffindors.append({"name": student, "house": "Gryffindor"})
+gryffindors = {student: "Gryffindor" for student in students}
 
 print(gryffindors)
