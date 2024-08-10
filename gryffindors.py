02898206c93e664ed902a97ecbe99b068cdec569
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-10 11:29:49 +0100

    filter() on is gryffindor

--- gryffindors.py
+++ gryffindors.py
@@ -5,9 +5,17 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
 ]
 
-gryffindors = [
-    student["name"] for student in students if student["house"] == "Gryffindor"
-]
 
-for gryffindor in sorted(gryffindors):
-    print(gryffindors)
+def is_gryffindor(s):
+    # Here I am returning the value of the boolean in a simplified manner
+    # If the student house is gryffindor, return. If False, do nothing. That's basically what it's saying
+    return s["house"] == "Gryffindor"
+
+
+# Similar to map, I am passing a function that is going to apply to every student in my list
+# The filter() function returns an iterator where the items are filtered through a function to test if the item is accepted or not
+gryffindors = filter(is_gryffindor, students)
+
+# I can sort a list in a dictionary using the below syntax
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
