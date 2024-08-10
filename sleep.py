7df15ae15ec77a3e8cdb74a5991d5a1b678d8e7b
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-10 12:05:06 +0100

    Introducing yield

--- sleep.py
+++ sleep.py
@@ -1,11 +1,16 @@
 def main():
     n = int(input("What's n? "))
-    for i in range(n):
-        print(sheep(i))
+    for s in sheep(n):
+        print(s)
 
 
 def sheep(n):
-    return "🐑" * n
+    flock = []
+    for i in range(n):
+        # yield is telling the program: "Return one value at a time", rather than having the program waiting to generate all of them to print them in the terminal
+        # this will be useful to print very large amounts of sheep
+        yield "🐑" * i
+    return flock
 
 
 if __name__ == "__main__":
