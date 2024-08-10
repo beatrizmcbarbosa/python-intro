f4f0434f3baa7a04c2535e883d6744f42a632654
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2024-08-10 12:30:32 +0100

    pyttsx3 library

--- say.py
+++ say.py
@@ -1,15 +1,10 @@
-"""import cowsay"""
-import sys
+import cowsay
+import pyttsx3
 
-from sayings import hello
 
-if len(sys.argv) == 2:
-    hello(sys.argv[1])
-
-# Cow saying with Trex option
-"""if len(sys.argv) == 2:
-    cowsay.trex("hello, " + sys.argv[1])"""
-
-# Cow option
-"""if len(sys.argv) == 2:
-    cowsay.cow("hello, " + sys.argv[1])"""
+# Initialise pyttsx3 library
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
